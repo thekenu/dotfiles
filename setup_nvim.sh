da11526b75e4af5fe5be2638fd53ec3f861a1237
thekenu <thekenu@gmail.com>
2020-07-15 13:31:58 -0700

    Install ccls and ctags

--- setup_nvim.sh
+++ setup_nvim.sh
@@ -27,6 +27,38 @@ installripgrep() {
   sudo apt-get install -y ripgrep
 }
 
+installccls() {
+  # This function assumes Ubuntu 20.04, whose apt-get includes LLVM version 10.
+  # This is important because the CMake command hardcodes the LLVM path using
+  # the version number.
+  if ! lsb_release -a | grep -q "20.04"; then
+    echo "installccls only supports Ubuntu 20.04 at the moment"
+    exit 1
+  fi
+
+  # Install dependencies
+  sudo apt install -y zlib1g-dev libncurses-dev libncurses5 rapidjson-dev
+
+  # Install toolchain for building ccls from source
+  sudo apt install -y cmake clang libclang-dev
+
+  git clone --depth=1 --recursive https://github.com/MaskRay/ccls ~/ccls
+
+  cd ~/ccls
+
+  cmake -H. -BRelease -DCMAKE_BUILD_TYPE=Release \
+      -DCMAKE_PREFIX_PATH=/usr/lib/llvm-10 \
+      -DLLVM_INCLUDE_DIR=/usr/lib/llvm-10/include \
+      -DLLVM_BUILD_INCLUDE_DIR=/usr/include/llvm-10/
+  sudo cmake --build Release --target install
+
+  rm -rf ~/ccls
+}
+
+installctags() {
+  sudo apt install -y universal-ctags
+}
+
 linknvim() {
   ln -s $(pwd)/nvim ~/.config/nvim
 }
@@ -47,6 +79,8 @@ which node > /dev/null && echo 'node installed, moving on...' || installnode
 # Install extra dependecies
 which fzf > /dev/null && echo 'fzf installed, moving on...' || installfzf
 which rg > /dev/null && echo 'rg installed, moving on...' || installripgrep
+which ccls > /dev/null && echo 'ccls installed, moving on...' || installccls
+which ctags > /dev/null && echo 'ctags installed, moving on...' || installctags
 
 # Install nvim plugins
 nvim --headless +PlugInstall +CocUpdate +qall > /dev/null 2>&1
