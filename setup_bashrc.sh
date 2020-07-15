16f3099d83fce808ca74882b0154a0bbab1a46c2
Your Name <you@example.com>
2020-07-15 15:53:35 -0700

    Tweak bashrc script

--- setup_bashrc.sh
+++ setup_bashrc.sh
@@ -11,13 +11,13 @@ linksharedbashrc() {
 echo '[Setting up shared bashrc]'
 echo '==================='
 
-linksharedbashrc
+[[ -f "/etc/bashrc" ]] && echo '/etc/bashrc exists, moving on...' || linksharedbashrc
 
 echo -e "Add the following to ~/.bashrc source global definitions:"
 echo -e ""
-echo -e "  if [ -f /etc/bashrc ]; then"
-echo -e "      . /etc/bashrc"
-echo -e "  fi"
+echo -e "if [ -f /etc/bashrc ]; then"
+echo -e "  . /etc/bashrc"
+echo -e "fi"
 
 echo '==================='
 echo 'Done!'
