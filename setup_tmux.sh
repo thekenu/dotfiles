3598c775f9e6e29ea18f3f55726506c163c7f67c
Your Name <you@example.com>
2020-07-15 14:57:03 -0700

    Better error checking for tmux source

--- setup_tmux.sh
+++ setup_tmux.sh
@@ -49,7 +49,7 @@ pip3 show powerline-status > /dev/null && echo 'powerline installed, moving on..
 [[ -f "$HOME/.tmux.conf" ]] && echo '.tmux.conf exists, moving on...'|| linktmuxconf
 
 # Doesn't hurt to source .tmux.conf multiple times
-tmux source ~/.tmux.conf
+tmux source ~/.tmux.conf || echo 'tmux source failed, did you run this inside tmux?' && exit 1
 
 echo "Don't forget to prefix + I to insetall tmux plugins"
 
