13be73110b98469efcccb13d2ade9a3ae5c5942c
Ming Chen Lin <thekenu@gmail.com>
2020-07-21 15:39:18 -0700

    Add screenfetch

--- setup_i3-gaps.sh
+++ setup_i3-gaps.sh
@@ -10,7 +10,7 @@ installpackages() {
     sudo apt update
   fi
 
-  for package in i3 pavucontrol pasystray rofi compton i3blocks
+  for package in i3 pavucontrol pasystray rofi compton i3blocks screentfetch
   do
     which $package > /dev/null && echo $package 'installed, moving on...' \
       || sudo apt install $package
