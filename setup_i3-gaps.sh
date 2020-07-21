325314faf78dfdd3eefb8c0c9377802edab568c7
Ming Chen Lin <thekenu@gmail.com>
2020-07-21 13:06:22 -0700

    Simplify i3 script

--- setup_i3-gaps.sh
+++ setup_i3-gaps.sh
@@ -3,11 +3,18 @@
 set -o nounset    # error when referencing undefined variable
 set -o errexit    # exit when command fails
 
-installi3() {
-  sudo add-apt-repository ppa:kgilmer/speed-ricer
-  sudo apt update
-  sudo apt install i3-gaps
-  sudo apt install pavucontrol pasystray rofi compton
+installpackages() {
+  if ! find /etc/apt/ -name *.list | xargs cat | grep "^[[:space:]]*deb" | \
+       grep -q "kgilmer/speed-ricer"; then
+    sudo add-apt-repository ppa:kgilmer/speed-ricer
+    sudo apt update
+  fi
+
+  for package in i3 pavucontrol pasystray rofi compton i3blocks
+  do
+    which $package > /dev/null && echo $package 'installed, moving on...' \
+      || sudo apt install $package
+  done
 }
 
 linki3() {
@@ -22,7 +29,7 @@ linkrofi() {
 echo '[Setting up i3-gaps]'
 echo '==================='
 
-which i3 > /dev/null && echo 'i3 installed, moving on...' || installi3
+installpackages
 
 [[ -d "$HOME/.config/i3" ]] && echo 'i3 folder exists, moving on...'|| linki3
 [[ -d "$HOME/.config/rofi" ]] && echo 'rofi folder exists, moving on...'|| linkrofi
