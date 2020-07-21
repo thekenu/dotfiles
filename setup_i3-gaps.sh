6054ae2804af9d439802daa89853363bb216e0a4
Ming Chen Lin <thekenu@gmail.com>
2020-07-21 12:38:23 -0700

    Tweak i3 for rofi

--- setup_i3-gaps.sh
+++ setup_i3-gaps.sh
@@ -7,10 +7,15 @@ installi3() {
   sudo add-apt-repository ppa:kgilmer/speed-ricer
   sudo apt update
   sudo apt install i3-gaps
+  sudo apt install pavucontrol pasystray rofi compton
 }
 
 linki3() {
   ln -s $(git rev-parse --show-toplevel)/i3 ~/.config/i3
+}
+
+linkrofi() {
+  ln -s $(git rev-parse --show-toplevel)/rofi ~/.config/rofi
 }
 
 # Welcome
@@ -20,6 +25,7 @@ echo '==================='
 which i3 > /dev/null && echo 'i3 installed, moving on...' || installi3
 
 [[ -d "$HOME/.config/i3" ]] && echo 'i3 folder exists, moving on...'|| linki3
+[[ -d "$HOME/.config/rofi" ]] && echo 'rofi folder exists, moving on...'|| linkrofi
 
 echo '==================='
 echo 'Done!'
